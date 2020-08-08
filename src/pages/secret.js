6983e07ad8b9e7eb015f42194ff354877b0c8fe3
Jason Lengstorf <jason@lengstorf.com>
2020-08-07 18:14:37 -0700

    feat: add client-side access checks

--- src/pages/secret.js
+++ src/pages/secret.js
@@ -1,18 +1,31 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import netlifyIdentity from "netlify-identity-widget"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 
 export default function Secret() {
+  const [isAllowed, setIsAllowed] = useState(false)
+
   useEffect(() => {
     netlifyIdentity.init()
+
+    const user = netlifyIdentity.currentUser()
+    const roles = user?.app_metadata?.roles || []
+
+    if (roles.includes("user")) {
+      setIsAllowed(true)
+      return
+    }
+
+    // not logged in — bail to login page
+    navigate("/login")
   }, [])
 
-  return (
+  return isAllowed ? (
     <div>
       <h1>Secrets!</h1>
       <p>You should only be able to see this if you’re logged in.</p>
       <Link to="/">back to the home page</Link>
       <div data-netlify-identity-menu />
     </div>
-  )
+  ) : null
 }
