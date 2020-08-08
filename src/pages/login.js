a1b0c3733fc43ad86efd7b7a93a6b62bee1f8749
Jason Lengstorf <jason@lengstorf.com>
2020-08-07 17:50:24 -0700

    feat: add redirects

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,15 +2,15 @@ import React, { useEffect } from "react"
 import netlifyIdentity from "netlify-identity-widget"
 import { Link } from "gatsby"
 
-export default function Secret() {
+export default function Login() {
   useEffect(() => {
     netlifyIdentity.init()
   }, [])
 
   return (
     <div>
-      <h1>Secrets!</h1>
-      <p>You should only be able to see this if you’re logged in.</p>
+      <h1>Log In</h1>
+      <p>Log in to learn secrets!</p>
       <Link to="/">back to the home page</Link>
       <div data-netlify-identity-menu />
     </div>
